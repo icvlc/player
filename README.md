7b81bd5cc9068767572b419c7a5bd63af06890e9
PW486 <ooqwe486@gmail.com>
2019-10-24 16:11:13 +0900

    README와 라이센스 추가, 코드 수정

---
+++ README.md
@@ -0,0 +1,18 @@
+# ICVLC Player
+
+> Inter-Car Visible Light Communication (Player)
+
+시리얼 포트로 읽은 메시지를 TTS(Text to Speech)를 이용하여 출력하는 프로그램
+
+## Usage
+
+먼저, **receiver.py**의 포트를 알맞게 수정합니다. (기본 값: COM3)
+
+```sh
+$ pip install -r requirements.txt
+$ python receiver.py
+```
+
+## License
+
+[MIT](https://github.com/icvlc/player/blob/master/LICENSE)
--- receiver.py
+++ receiver.py
@@ -2,19 +2,22 @@ from serial import Serial
 from gtts import gTTS
 from winmm import play
 
-ser = Serial('COM3', timeout=0.5)
+port = 'COM3'
+ser = Serial(port, timeout=0.5)
 buf = b''
 
 while True:
-    b = ser.read()
-    if not b:
-        continue
-    print(f'\rbuffer: {str(buf).ljust(80)}', end='')
-    if b == b'\n':
-        msg = buf.decode()
-        r = gTTS(text=msg, lang='ko')
-        r.save('out.mp3')
-        play('out.mp3')
-        buf = b''
-    elif b != b'\r':
-        buf += b
+  data = ser.read()
+  if not data:
+    continue
+
+  print(f'\rbuffer: {str(buf).ljust(80)}', end='')
+
+  if data == b'\n':
+    msg = buf.decode()
+    tts = gTTS(text=msg, lang='ko')
+    tts.save('out.mp3')
+    play('out.mp3')
+    buf = b''
+  elif data != b'\r':
+    buf += data
