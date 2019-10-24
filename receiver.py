97101b859f9da47c21b6b77863362fb54370f1cd
Hanjun Kim <hallazzang@gmail.com>
2019-10-25 08:33:58 +0900

    출력 개선

--- receiver.py
+++ receiver.py
@@ -1,23 +1,41 @@
+import sys
+
 from serial import Serial
 from gtts import gTTS
 from winmm import play
 
-port = 'COM3'
-ser = Serial(port, timeout=0.5)
+ser = Serial('COM3', timeout=0.5)
 buf = b''
+msg = ''
 
 while True:
-  data = ser.read()
-  if not data:
-    continue
+    print(f'\rmsg: {msg.ljust(40)}', end='')
+    sys.stdout.flush()
 
-  print(f'\rbuffer: {str(buf).ljust(80)}', end='')
+    data = ser.read()
+    if not data:
+        continue
 
-  if data == b'\n':
-    msg = buf.decode()
-    tts = gTTS(text=msg, lang='ko')
-    tts.save('out.mp3')
-    play('out.mp3')
-    buf = b''
-  elif data != b'\r':
-    buf += data
+    try:
+        msg = buf.decode()
+    except UnicodeDecodeError:
+        pass
+
+    if data == b'\n':
+        try:
+            msg = buf.decode()
+        except UnicodeDecodeError:
+            print('\nIgnoring...')
+            buf = b''
+            msg = ''
+        else:
+            print()
+
+        tts = gTTS(text=msg, lang='ko')
+        tts.save('out.mp3')
+        play('out.mp3')
+
+        buf = b''
+        msg = ''
+    elif data != b'\r':
+        buf += data
